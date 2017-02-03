b96953bc5ca111555df26dd40700b17330870754
Craig Weidert <craig.weidert@gmail.com>
2017-02-02 16:39:38 -0800

    put update file in

---
+++ src/com/heliomug/games/space/gui/PanelReturnToGame.java
@@ -0,0 +1,21 @@
+package com.heliomug.games.space.gui;
+
+import java.awt.BorderLayout;
+import java.awt.event.KeyEvent;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+import com.heliomug.utils.gui.UpdatingButton;
+
+@SuppressWarnings("serial")
+public class PanelReturnToGame extends JPanel {
+	public PanelReturnToGame() {
+		super(new BorderLayout());
+		JButton button = new UpdatingButton("Return to Game", () -> {
+			SpaceFrame.setCard(SpaceFrame.GAME_CARD);
+		});
+		button.setMnemonic(KeyEvent.VK_G);
+		add(button);	
+	}
+}
